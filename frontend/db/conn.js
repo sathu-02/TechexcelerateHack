3a87733abb9010ccef21a083c044090da4e8c400
Vishwa <vishwateja2684@gmail.com>
2025-03-23 15:57:46 +0530

    revert

--- frontend/db/conn.js
+++ frontend/db/conn.js
@@ -1,35 +1,27 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://edithvision3000:edithvision@cluster0.dqjqi.mongodb.net/anomalydetectiondb?retryWrites=true&w=majority";
+//creating a database
+const connect = mongoose.connect("mongodb+srv://edithvision3000:edithvision@cluster0.dqjqi.mongodb.net/anomalydetectiondb?retryWrites=true&w=majority");
 
-let isConnected = false;
-
-const connectDB = async () => {
-    if (isConnected) {
-        console.log("Using existing database connection");
-        return;
-    }
-
-    try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-
-        isConnected = true;
-        console.log("Database connected successfully");
-    } catch (error) {
-        console.error("Database connection failed:", error);
-        throw error;
-    }
-};
-
-// Define Schema & Model
-const Loginschema = new mongoose.Schema({
-    name: { type: String, required: true },
-    password: { type: String, required: true }
+connect.then(()=>{
+    console.log("connection successful");
+}).catch((error)=>{
+    console.log(error);
 });
 
-const User = mongoose.model("users", Loginschema);
+//create schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
 
-module.exports = { connectDB, User };
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
